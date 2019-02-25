7d5fa3a0c0b1ee5fc633c1c616cb17dbcf93a5a7
Zefeng Li <li.zefeng8@gmail.com>
2019-02-25 09:35:43 +0800

    Create 01.py

---
+++ 027. Remove Element/python/01.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        next=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[next]=nums[i]
+                next+=1
+        return next
