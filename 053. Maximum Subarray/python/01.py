c23013fddd73b91a204699b4fb6a4d11bca2b1fe
Zefeng Li <li.zefeng8@gmail.com>
2019-02-27 07:38:12 +0800

    Create 01.py

---
+++ 053. Maximum Subarray/python/01.py
@@ -0,0 +1,28 @@
+"""
+WRONG:
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        subarray=[]
+        sum=0
+        for i in range(len(nums)):
+            if not subarray or (nums[i]>0 or nums[i]==0):
+                subarray.append(nums[i])
+                sum+=nums[i]
+            elif nums[i]>subarray[0]:
+                sum=sum+(nums[i]-subarray[0])
+                del subarray[0:1]
+                subarray.append(nums[i])  
+        return sum
+"""
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        overall_max=float('-inf')
+        max_ending_here=0
+        for num in nums:
+            if max_ending_here>0:
+                max_ending_here+=num
+            else:
+                max_ending_here=num
+            overall_max=max(overall_max,max_ending_here)
+        return overall_max
