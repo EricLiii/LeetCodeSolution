12a29c3557b4210a6c0d43ca45b76f64b7f18c67
Zefeng Li <li.zefeng8@gmail.com>
2019-02-28 12:45:20 +0800

    Update 01.py

--- 066. Plus One/python/01.py
+++ 066. Plus One/python/01.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution_1:
     def plusOne(self, digits: List[int]) -> List[int]:
         i=len(digits)-1
         forward=True
@@ -12,3 +12,13 @@ class Solution:
         if forward==True:
             digits.insert(0,1)
         return digits
+
+class Solution_2:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        i=len(digits)-1
+        while i>=0 and digits[i]==9:
+            digits[i]=0
+            i-=1
+        if i==-1:
+            return [1]+digits
+        return digits[:i]+[digits[i]+1]+digits[i+1:]
