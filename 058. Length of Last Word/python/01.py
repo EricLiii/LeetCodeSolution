71b660c555361ba26a2dea00da0d2d5ba0263e3f
Zefeng Li <li.zefeng8@gmail.com>
2019-02-28 08:29:19 +0800

    Update 01.py

--- 058. Length of Last Word/python/01.py
+++ 058. Length of Last Word/python/01.py
@@ -6,6 +6,7 @@ class Solution:
             if s[i]==' ' and end!=-1:
                 return end-i
             if s[i]!=' ' and end ==-1:
-                end =i
+                end =i                  # find the first letter from the end of string s.
             i-=1
-        return end+1 if end!=-1 else 0
+        return end+1 if end!=-1 else 0  # if end==-1: the string is empty or only consists of spaces.
+                                        # if end!=-1: the string s doesn't have any space OR only has spaces at the end.
