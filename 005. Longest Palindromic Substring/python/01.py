a1a6cb81455401d33d185d1818651b2598cf2870
Zefeng Li <li.zefeng8@gmail.com>
2019-03-18 22:36:55 +0800

    Create 01.py

---
+++ 005. Longest Palindromic Substring/python/01.py
@@ -0,0 +1,41 @@
+class Solution:
+    ''' 
+    68ms
+    '''
+    def longestPalindrome(self, s: str) -> str:
+        if len(s)==0:
+        	return ""
+        maxLen=1
+        start=0
+        for i in range(len(s)):
+        	if i-maxLen >=1 and s[i-maxLen-1:i+1]==s[i-maxLen-1:i+1][::-1]:
+        		start=i-maxLen-1
+        		maxLen+=2
+        		continue
+
+        	if i-maxLen >=0 and s[i-maxLen:i+1]==s[i-maxLen:i+1][::-1]:
+        		start=i-maxLen
+        		maxLen+=1
+        return s[start:start+maxLen]
+        
+class Solution:
+    '''
+    912ms, why?
+    '''
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
