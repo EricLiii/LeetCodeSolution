777430fbea0a3c3913cd4eec729f1cf928a99fdf
Zefeng Li <li.zefeng8@gmail.com>
2019-03-19 09:03:36 +0800

    Create 01.py

---
+++ 008. String to Integer (atoi)/python/01.py
@@ -0,0 +1,22 @@
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        str=str.strip()
+        neg=False
+        if str and str[0] =='-':
+            neg=True
+        if str and (str[0]=='+' or str[0]=='-'):             
+            str=str[1:]
+        if not str:
+            return 0
+        
+        digits={i for i in '0123456789'}
+        result=0
+        for c in str:
+            if c not in digits:
+                break
+            result=result*10+int(c)
+        if neg:
+            result=-result
+        result=max(min(result,2**31-1),-2**31)
+        return result
+                
