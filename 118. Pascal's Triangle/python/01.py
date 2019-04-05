34849f40e9351008a9b9828d92b1f66b9d599479
Zefeng Li <li.zefeng8@gmail.com>
2019-04-05 09:05:44 +0800

    Create 01.py

---
+++ 118. Pascal's Triangle/python/01.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows==0:
+            return []
+        pascal=[[1]]
+        for i in range(1,numRows):
+            pascal.append([1])
+            for num1, num2 in zip(pascal[-2][:-1], pascal[-2][1:]):
+                pascal[-1].append(num1+num2)
+            pascal[-1].append(1)
+        return pascal
