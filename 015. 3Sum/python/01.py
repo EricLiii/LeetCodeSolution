dc33990d6f067dc47f7afabc48bd8c9dfa702576
zefeng <li.zefeng8@gmail.com>
2019-07-08 22:58:07 +0800

    add new

--- 015. 3Sum/python/01.py
+++ 015. 3Sum/python/01.py
@@ -1,5 +1,9 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+    """
+    Runtime: 908 ms, faster than 65.63% of Python3 online submissions for 3Sum.
+    Memory Usage: 16.6 MB, less than 89.71% of Python3 online submissions for 3Sum.
+    """
         res = []
         nums.sort()
         for i in range(len(nums)-2):
@@ -13,7 +17,8 @@ class Solution:
                 elif s > 0:
                     r -= 1
                 else:
-                    res.append((nums[i], nums[l], nums[r]))
+                    res.append((nums[i], nums[l], nums[r])) #这里添加的是tuple，最后返回的是一个由tuple组成的list，不知道网站为什么最后还是接受了答案。
+                    #不过append tuple 确实比append list 快一点。
                     while l < r and nums[l] == nums[l+1]:
                         l += 1
                     while l < r and nums[r] == nums[r-1]:
--- 016. 3Sum Closest/python/01.py
+++ 016. 3Sum Closest/python/01.py
@@ -1,4 +1,8 @@
 class Solution:
+"""
+Runtime: 116 ms, faster than 64.64% of Python3 online submissions for 3Sum Closest.
+Memory Usage: 13.2 MB, less than 33.86% of Python3 online submissions for 3Sum Closest.
+"""
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         result=nums[0]+nums[1]+nums[2]
         nums.sort()
@@ -14,5 +18,33 @@ class Solution:
                     l += 1
                 else:
                     r -= 1
-        return result       
+        return result     
+
+
+class Solution:
+"""
+Author: Zefeng
+
+Runtime: 132 ms, faster than 43.12% of Python3 online submissions for 3Sum Closest.
+Memory Usage: 13.3 MB, less than 10.53% of Python3 online submissions for 3Sum Closest.
+"""
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if len(nums) <= 2:
+            return None
+        nums.sort()
+        res = nums[0] + nums[1] + nums[len(nums)-1]
+        for i in range(len(nums)-2):
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] +nums[r]
+                if s - target == 0:
+                    return s
+                else:
+                    if abs(s - target) < abs(res - target):
+                        res = s
+                    if s - target > 0:
+                        r -=1
+                    else:
+                        l += 1
+        return res        
                     
