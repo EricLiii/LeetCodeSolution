e71581b7db5e108d67d3aecfa933d87d54b5aff2
zefeng <li.zefeng8@gmail.com>
2019-07-30 23:01:04 +0800

    add new

---
+++ 0138. Copy List with Random Pointer/python/0138.py
@@ -0,0 +1,46 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution_1:
+"""
+Runtime: 40 ms, faster than 98.32% of Python3 online submissions for Copy List with Random Pointer.
+Memory Usage: 16.2 MB, less than 5.17% of Python3 online submissions for Copy List with Random Pointer.
+"""
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        dic = dict()
+        m = n = head
+        while m:
+            dic[m] = Node(m.val, None, None)
+            m = m.next
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+        return dic.get(head)
+        
+class Solution_2:
+"""
+很好的想法，值得学习！
+
+Runtime: 44 ms, faster than 91.60% of Python3 online submissions for Copy List with Random Pointer.
+Memory Usage: 16.2 MB, less than 5.17% of Python3 online submissions for Copy List with Random Pointer.
+
+Idea:
+collections.defaultdict 的用法：https://www.cnblogs.com/zhizhan/p/5692668.html
+
+"""
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        dic = collections.defaultdict(lambda: Node(0, None, None)) #默认的Node为Node(0, None, None).
+        dic[None] = None  #需要先指定key为None时的value,否则当next为None时，会生成一个新的Node，而不是指向None.
+        n = head
+        while n:
+            dic[n].val = n.val
+            dic[n].next = dic[n.next]
+            dic[n].random = dic[n.random]
+            n = n.next
+        return dic[head]
