e89cb9d3fde7d295ea551e35348c5430849975f5
Li Zefeng <lzf940514@gmail.com>
2019-08-01 20:37:04 +0800

    add new

--- 0002. Add Two Numbers/python/0002.py
+++ 0002. Add Two Numbers/python/0002.py
@@ -1,4 +1,8 @@
 def addTwoNumbers(self, l1, l2):
+"""
+Runtime: 80 ms, faster than 56.03% of Python3 online submissions for Add Two Numbers.
+Memory Usage: 14.1 MB, less than 5.13% of Python3 online submissions for Add Two Numbers.
+"""
     dummy = cur = ListNode(0)
     carry = 0
     while l1 or l2 or carry:
@@ -12,3 +16,49 @@ def addTwoNumbers(self, l1, l2):
         cur = cur.next
         carry //= 10
     return dummy.next
+    
+class Solution:
+"""
+Author: Zefeng
+
+Runtime: 76 ms, faster than 78.39% of Python3 online submissions for Add Two Numbers.
+Memory Usage: 13.9 MB, less than 5.13% of Python3 online submissions for Add Two Numbers.
+"""
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        dummy = head = ListNode(0)
+        dummy.next = head
+        while l1 or l2 or carry:
+            if not l1 and not l2 and carry:
+                head.next = ListNode(carry)
+                break
+            if l1 and l2:
+                summ = l1.val + l2.val +carry
+                if summ > 9:
+                    summ = summ - 10
+                    carry = 1
+                else:
+                    carry = 0
+                head.next = ListNode(summ)
+                l1 = l1.next
+                l2 = l2.next
+            elif l1 and not l2:
+                summ = l1.val + carry
+                if summ > 9:
+                    summ = summ -10
+                    carry = 1
+                else:
+                    carry = 0
+                head.next = ListNode(summ)
+                l1 = l1.next
+            else:
+                summ = l2.val + carry
+                if summ > 9:
+                    summ = summ -10
+                    carry = 1
+                else:
+                    carry = 0
+                l2 = l2.next
+                head.next = ListNode(summ)
+            head = head.next
+        return dummy.next
