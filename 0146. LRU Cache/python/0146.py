0f3b84094eb0cbec4f85b885904fe75f55f01cac
zefeng <li.zefeng8@gmail.com>
2019-09-12 23:37:20 +0800

    add new

--- 0146. LRU Cache/python/0146.py
+++ 0146. LRU Cache/python/0146.py
@@ -1,29 +1,104 @@
-class LRUCache:
+class LRUCache_1:
+"""
+Runtime: 500 ms, faster than 12.73% of Python3 online submissions for LRU Cache.
+Memory Usage: 22.8 MB, less than 6.06% of Python3 online submissions for LRU Cache.
 
-#Wrong solution
-
+这个方法不是O(1)的. 面试时如果对时间有要求不能写这个.
+"""
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.queue = []
-        self.map = {}
         self.dic = {}
-        
+
     def get(self, key: int) -> int:
         if key in self.dic:
-            k = self.queue.pop(self.map[key])
-            self.queue.append(k)
-            self.map[k] = len(self.queue)-1
+            self.queue.remove(key)
+            self.queue.append(key)
             return self.dic[key]
         else: return -1
 
     def put(self, key: int, value: int) -> None:
+        if key in self.dic: #要先判断在不在，再判断大小够不够,否则会出问题.
+            self.queue.remove(key)
+            self.queue.append(key)
+            self.dic[key] = value
+            return
         if len(self.dic) >= self.capacity:
             k = self.queue.pop(0)
             self.dic.pop(k)
-            self.map.pop(k)
-        if key in self.queue:
-            self.queue.pop(self.map[key])
-
         self.queue.append(key)
         self.dic[key] = value
-        self.map[key] = len(self.queue)-1
+        
+        class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+        
+class LRUCache_2:
+"""
+Runtime: 256 ms, faster than 27.76% of Python3 online submissions for LRU Cache.
+Memory Usage: 23.2 MB, less than 6.06% of Python3 online submissions for LRU Cache.
+
+Link: https://leetcode.com/problems/lru-cache/discuss/202067/Python-or-O(1)-tm-146
+https://www.youtube.com/watch?v=S6IfqDXWa10
+
+第一个链接采用将新的node放在后面，而我的code是将新的node放在前面，和第二个视频链接一致。
+"""
+    def __init__(self, capacity: int):
+        self.dic = {}
+        self.capacity = capacity
+        self.dummy_head = Node(0, 0)
+        self.dummy_tail = Node(0, 0)
+        self.dummy_head.next = self.dummy_tail
+        self.dummy_tail.prev = self.dummy_head
+
+    def get(self, key: int) -> int:
+        if key not in self.dic:
+            return -1
+        node = self.dic[key]
+        self.remove(node)
+        self.insert(node)
+        return self.dic[key].val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(node)   
+        node = Node(key, value)
+        self.insert(node)
+        self.dic[key] = node
+        if len(self.dic) > self.capacity: #如果是先改value再判断大小，就只能判断是否大于.
+            tail = self.dummy_tail.prev
+            self.remove(tail)
+            del self.dic[tail.key]
+    """
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(node)
+            self.insert(node)
+            self.dic[key].val = value
+            return 
+        if len(self.dic) == self.capacity: #如果是先判断大小再改value,那么在判断大小之前的情况要及时return
+            tail = self.dummy_tail.prev
+            self.remove(tail)
+            del self.dic[tail.key]
+        node = Node(key, value)
+        self.insert(node)
+        self.dic[key] = node
+        
+    """    
+    def insert(self, node):
+        head = self.dummy_head.next
+        self.dummy_head.next = node
+        node.prev = self.dummy_head
+        node.next = head
+        head.prev = node
+    
+    def remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
--- 0200. Number of Islands/python/0200.py
+++ 0200. Number of Islands/python/0200.py
@@ -27,3 +27,59 @@ dfs.
         self.dfs(grid, i-1, j)
         self.dfs(grid, i, j+1)
         self.dfs(grid, i, j-1)
+        
+class Solution_2:
+"""
+Runtime: 188 ms, faster than 17.95% of Python3 online submissions for Number of Islands.
+Memory Usage: 15.1 MB, less than 9.40% of Python3 online submissions for Number of Islands.
+
+bfs.
+"""
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        self.m, self.n = len(grid), len(grid[0])
+        self.queue = []
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.queue.append((i, j))
+                    self.bfs(grid)
+                    count += 1
+        return count
+        
+    def bfs(self, grid):
+        while self.queue:
+            i, j = self.queue.pop(0) #往后放，从头取.
+            if i < 0 or j < 0 or i >= self.m or j >= self.n or grid[i][j] == "0":
+                continue
+            if grid[i][j] == "1":
+                grid[i][j] = "0"
+                self.queue += [item for item in [(i-1,j), (i+1,j), (i,j-1), (i,j+1)]]
+                self.bfs(grid)
+                
+class Solution_3:
+"""
+Runtime: 168 ms, faster than 41.92% of Python3 online submissions for Number of Islands.
+Memory Usage: 14.6 MB, less than 9.40% of Python3 online submissions for Number of Islands.
+
+bfs,但是写在同一个函数里，更简洁.
+"""
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        queue = collections.deque()
+        steps = [[-1,0],[1,0],[0,-1],[0,1]]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=='1' : 
+                    count+=1
+                    grid[i][j] = '0'
+                    queue.append((i,j))
+                    while queue:
+                        x,y = queue.popleft()
+                        for step in steps:
+                            if 0<= x+step[0]<len(grid) and 0<=y+step[1]<len(grid[0]) and grid[x+step[0]][y+step[1]] == '1':
+                                grid[x+step[0]][y+step[1]] = "0"
+                                queue.append((x+step[0] ,y+step[1]))
+        return count
