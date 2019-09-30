87ecca27ddaa83a4ece3b830345bab70df454ee9
zefeng <li.zefeng8@gmail.com>
2019-09-30 23:02:52 +0800

    add new

---
+++ 0202. Happy Number/python/0202.py
@@ -0,0 +1,41 @@
+class Solution_1:
+"""
+Author: Zefeng
+
+Runtime: 44 ms, faster than 46.46% of Python3 online submissions for Happy Number.
+Memory Usage: 13.8 MB, less than 5.26% of Python3 online submissions for Happy Number.
+"""
+    def isHappy(self, n: int) -> bool:
+        self.lst = []
+        return self.helper(n)
+    
+    def helper(self, n):
+        if n == 1:
+            return True
+        summ = 0
+        while n:
+            summ += (n % 10)**2
+            n = n // 10
+        if summ in self.lst:
+            return False
+        else:
+            self.lst.append(summ)
+        return self.helper(summ)
+        
+class Solution_2:
+"""
+Runtime: 44 ms, faster than 46.46% of Python3 online submissions for Happy Number.
+Memory Usage: 13.7 MB, less than 5.26% of Python3 online submissions for Happy Number.
+
+更简洁，不需要定义新的函数.
+"""
+    def isHappy(self, n: int) -> bool:
+        mem = set()
+        while n != 1:
+            n = sum([int(i) ** 2 for i in str(n)])
+            if n in mem:
+                return False
+            else:
+                mem.add(n)
+        else:
+            return True
---
+++ 0204. Count Primes/python/0204.py
@@ -0,0 +1,35 @@
+Link: https://blog.csdn.net/github_39261590/article/details/73864039
+
+class Solution_1:
+"""
+Runtime: 264 ms, faster than 84.40% of Python3 online submissions for Count Primes.
+Memory Usage: 37.1 MB, less than 37.93% of Python3 online submissions for Count Primes.
+"""
+    def countPrimes(self, n: int) -> int:
+        if n < 3: #因为求的是小于n的质数，所以n=2时结果也是0.
+            return 0
+        primes = [True] * n
+        primes[0] = primes[1] = False
+        for i in range(2, int(n ** 0.5) + 1): # n的最小的非质因子一定小于n的平方根.
+            if primes[i]:
+                #要从i的两倍开始，否则第i项也成False了。
+                primes[i*2: n: i] = [False] * len(primes[i*2: n: i]) 
+        return sum(primes)
+
+
+class Solution_2:
+"""
+Runtime: 224 ms, faster than 88.03% of Python3 online submissions for Count Primes.
+Memory Usage: 37.1 MB, less than 37.93% of Python3 online submissions for Count Primes.
+"""
+def countPrimes(self, n):
+    if n < 3:
+        return 0
+    primes = [True] * n
+    primes[0] = primes[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            #其实没必要从i*2开始，因为i*2 = 2*i, 在i=2的时候已经处理过了.
+            primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
+    return sum(primes)
+    
