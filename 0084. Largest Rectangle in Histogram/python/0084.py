b8187677c074396985d69a9e3ceff03ebd071ec9
Li Zefeng <li.zefeng8@gmail.com>
2021-01-07 07:05:27 +0800

    add all

--- 0084. Largest Rectangle in Histogram/python/0084.py
+++ 0084. Largest Rectangle in Histogram/python/0084.py
@@ -14,8 +14,8 @@ https://www.cnblogs.com/grandyang/p/4322653.html
         for i in range(len(heights)):
             while heights[i] < heights[stack[-1]]:
                 h = heights[stack.pop()]
+                # 注意这里的stack[-1]跟前面的已经不一样了，以为经历了stack.pop()
                 w = i - stack[-1] - 1
                 ans = max(ans, h * w)
             stack.append(i)
-        #heights.pop() 这一步没必要
         return ans
